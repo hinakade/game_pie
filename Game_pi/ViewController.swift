f694d555d40ce36075be9c63f5d1614b38175167
kadehina <geta206@gmail.com>
2016-02-06 23:11:39 +0800

    Initial Commit

---
+++ Game_pi/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Game_pi
+//
+//  Created by KadeHina on 2016/02/06.
+//  Copyright © 2016年 KadeHina. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
