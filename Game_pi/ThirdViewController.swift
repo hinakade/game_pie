619a9ab6e49f7b31dd0d198212326d8eb789586f
kadehina <geta206@gmail.com>
2016-02-21 18:52:22 +0800

    ０秒になったら結果の画面に移動する機能追加

--- Game_pi/ThirdViewController.swift
+++ Game_pi/ThirdViewController.swift
@@ -9,18 +9,32 @@
 import UIKit
 
 class ThirdViewController: UIViewController {
+    @IBOutlet weak var countdownLabel: UITextField!
+    
     
     //最初に表示されるスタート地点の数字
-    var timerCount = 120
+    var timerCount = 5
     var timer = NSTimer()
+
+
     
-    @IBOutlet weak var myLabel: UILabel!
     func Counting(){
         timerCount -= 1
-        myLabel.text = "\(timerCount)"
+        
+        countdownLabel.text = "残り  \(timerCount)秒"
+        
+        if(timerCount == 0){
+        // タイマーの停止
+        timer.invalidate()
+        //次の画面に勝手に移動する
+            var targetVi: AnyObject = self.storyboard!.instantiateViewControllerWithIdentifier( "equal" )
+            self.presentViewController(targetVi as! UIViewController, animated: true, completion: nil)
+
+            
+        }
+        
     }
-    
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
